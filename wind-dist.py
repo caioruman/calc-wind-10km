1a22515ee065b3fd98ae61c4dabe319f050cebf2
Caio J Ruman <caioruman@gmail.com>
2020-08-26 17:25:27 -0400

    Testing SHF

--- wind-dist.py
+++ wind-dist.py
@@ -23,7 +23,7 @@ import time
   - Open the RPN files for each month
   - Group the data in one array
   - Get the timeseries for each station
-  - Divide the timeseries between SHF > 0 and SHF < 0 and deltaT
+  - Divide the timeseries between SHF > 0 and SHF < 0 
   - K-mean clustering of those two timeseries
 '''
 
@@ -62,176 +62,113 @@ def main():
         print("Month already calculated. skipping.")
         continue
 
-      arq_dm = glob("{0}/Samples/{1}_{2}{3:02d}/dm*".format(main_folder, exp, year, month))[0]
-      arq_pm = glob("{0}/Samples/{1}_{2}{3:02d}/pm*".format(main_folder, exp, year, month))[0]  
+      arq_dm_month = glob("{0}/Samples/{1}_{2}{3:02d}/dm*".format(main_folder, exp, year, month)).sorted()
+      arq_pm_month = glob("{0}/Samples/{1}_{2}{3:02d}/pm*".format(main_folder, exp, year, month)).sorted()
 
-      # Reading SHF. File shape: (time, soil type, lat, lon)
-      with RPN(arq_pm) as r:
-        print("Opening file {0}".format(arq_pm))
-        shf = np.squeeze(r.variables["AHF"][:])
-        # I want the grid average, getting the field number 5
-        shf = shf[:,4,:,:]
-                
-        tskin = np.squeeze(r.variables["TSKN"][:]) - 273.15
-        tskin = tskin[:,4,:,:]
-        
-        if 'lons2d' not in locals():
-          lons2d, lats2d = r.get_longitudes_and_latitudes_for_the_last_read_rec()  
-            
-
-      # Reading Wind and Temperature (time, level, lat, lon)
-      with RPN(arq_dm) as r:
-        print("Opening file {0}".format(arq_dm))
-        uu = np.squeeze(r.variables["UU"][:])
-        vv = np.squeeze(r.variables["VV"][:])
-        print(uu.shape)
-        
-        uu_10 = uu[0]
-        vv_10 = vv[0]
-
-        tt = np.squeeze(r.variables["TT"][:])
-        tt_10 = tt[:,-1,:,:]  
-        print(tt.shape)
-        #print(tt[0,0, 150, 150])
-        #print(tt[0,-1, 150, 150])
-        utest = r.variables["UU"]
-        ttest = r.variables["TT"]
-        print([lev for lev in utest.sorted_levels])
-        print([lev for lev in ttest.sorted_levels])
-
-        #t2m = r.get_first_record_for_name("TT", label="PAN_ERAI_DEF")        
-                
-      uv = np.sqrt(np.power(uu, 2) + np.power(vv, 2))
-      uv_10 = np.sqrt(np.power(uu_10, 2) + np.power(vv_10, 2))
-      #print(uv[0,0, 150, 150])
-      #print(uv[0,-1, 150, 150])
+      print(arq_dm_month)
       sys.exit()
-      print(uu.shape, vv.shape, t2m.shape)  
 
-      # Reading the wind on preassure levels
+      for arq_dm, arq_pm in zip(arq_dm_month, arq_pm_month):
+      # Reading SHF. File shape: (time, soil type, lat, lon)
+        with RPN(arq_pm) as r:
+          print("Opening file {0}".format(arq_pm))
+          shf = np.squeeze(r.variables["AHF"][:])
+          # I want the grid average, getting the field number 5
+          shf = shf[:,4,:,:]
+                  
+          tskin = np.squeeze(r.variables["TSKN"][:]) - 273.15
+          tskin = tskin[:,4,:,:]
+          
+          if 'lons2d' not in locals():
+            lons2d, lats2d = r.get_longitudes_and_latitudes_for_the_last_read_rec()  
+              
 
-      with RPN(arq_dp) as r:
-        print("Opening file {0}".format(arq_dp))
-        uu_press = np.squeeze(r.variables["UU"][:])
-        vv_press = np.squeeze(r.variables["VV"][:]) 
+        # Reading Wind and Temperature (time, level, lat, lon)
+        with RPN(arq_dm) as r:
+          print("Opening file {0}".format(arq_dm))
+          uu = np.squeeze(r.variables["UU"][:])
+          vv = np.squeeze(r.variables["VV"][:])                        
 
-        tt_press = np.squeeze(r.variables["TT"][:]) 
+          tt = np.squeeze(r.variables["TT"][:])
+          tt_10 = tt[:,-1,:,:]  
+                  
+          #utest = r.variables["UU"]
+          #ttest = r.variables["TT"]
+          #print([lev for lev in utest.sorted_levels])
+          #print([lev for lev in ttest.sorted_levels])
 
-        levels = [lev for lev in r.variables["UU"].sorted_levels]
+          #t2m = r.get_first_record_for_name("TT", label="PAN_ERAI_DEF")        
+                  
+        uv = np.sqrt(np.power(uu, 2) + np.power(vv, 2))
+        uv_10 = uv[:,-1,:,:]
+        
+        sys.exit()
+              
+        lats = []
+        lons = []
+        stnames = []
 
-        dates_d = np.array(r.variables["UU"].sorted_dates)
+        stations = open('DatFiles/stations.txt', 'r')
+        for line in stations:
+          aa = line.replace("\n", '').split(';')
+          if (aa[0] != "#"):      
+            lats.append(float(aa[3]))
+            lons.append(float(aa[5]))
+            stnames.append(aa[1].replace(',',"_"))
 
-      print(tt_press.shape, uu_press.shape, vv_press.shape)
+        # looping throught all the stations
+        for lat, lon, name in zip(lats, lons, stnames):
 
-      uv_pressure = np.sqrt(np.power(uu_press, 2) + np.power(vv_press, 2))    
-      print(levels)
-      lats = []
-      lons = []
-      stnames = []
+          # Extract the info from the grid 
+          i, j = geo_idx([lat, lon], np.array([lats2d, lons2d]))
 
-      stations = open('/home/cruman/scratch/Scripts/calc-wind/stations.txt', 'r')
-      for line in stations:
-        aa = line.replace("\n", '').split(';')
-        if (aa[0] != "#"):      
-          lats.append(float(aa[3]))
-          lons.append(float(aa[5]))
-          stnames.append(aa[1].replace(',',"_"))
+          # Separating the negative and positive values, to apply to the wind
+          neg_shf = np.less_equal(shf[:, i, j], 0)
 
-      # looping throught all the stations
-      for lat, lon, name in zip(lats, lons, stnames):
+          neg_wind = uv[neg_shf, i, j]
+          pos_wind = uv[~neg_shf, i, j]
 
-        # Extract the info from the grid 
-        i, j = geo_idx([lat, lon], np.array([lats2d, lons2d]))
+          neg_t2m = t2m[neg_shf, i, j]
+          pos_t2m = t2m[~neg_shf, i, j]
 
-        # Separating the negative and positive values, to apply to the wind
-        neg_shf = np.less_equal(shf[:, i, j], 0)
+          neg_stemp = surf_temp[neg_shf, i, j]
+          pos_stemp = surf_temp[~neg_shf, i, j]
 
-        neg_wind = uv[neg_shf, i, j]
-        pos_wind = uv[~neg_shf, i, j]
+          neg_wind_press = uv_pressure[neg_shf, 10:, i, j]
+          pos_wind_press = uv_pressure[~neg_shf, 10:, i, j]
 
-        neg_t2m = t2m[neg_shf, i, j]
-        pos_t2m = t2m[~neg_shf, i, j]
+          neg_tt_press = tt_press[neg_shf, 10:, i, j]
+          pos_tt_press = tt_press[~neg_shf, 10:, i, j]
 
-        neg_stemp = surf_temp[neg_shf, i, j]
-        pos_stemp = surf_temp[~neg_shf, i, j]
+          neg_dates_d = dates_d[neg_shf]
+          pos_dates_d = dates_d[~neg_shf]
 
-        neg_wind_press = uv_pressure[neg_shf, 10:, i, j]
-        pos_wind_press = uv_pressure[~neg_shf, 10:, i, j]
+          df1 = pd.DataFrame(data=neg_wind_press, columns=levels[10:])
+          df2 = pd.DataFrame(data=pos_wind_press, columns=levels[10:])
 
-        neg_tt_press = tt_press[neg_shf, 10:, i, j]
-        pos_tt_press = tt_press[~neg_shf, 10:, i, j]
+          df1 = df1.assign(Dates=neg_dates_d)
+          df2 = df2.assign(Dates=pos_dates_d)
 
-        neg_dates_d = dates_d[neg_shf]
-        pos_dates_d = dates_d[~neg_shf]
+          df1.to_csv("{0}/CSV_RCP/{4}/{1}_{2}{3:02d}_windpress_neg.csv".format(folder, name, year, month, year))
+          df2.to_csv("{0}/CSV_RCP/{4}/{1}_{2}{3:02d}_windpress_pos.csv".format(folder, name, year, month, year))
 
-        df1 = pd.DataFrame(data=neg_wind_press, columns=levels[10:])
-        df2 = pd.DataFrame(data=pos_wind_press, columns=levels[10:])
+          df1 = pd.DataFrame(data=neg_tt_press, columns=levels[10:])
+          df2 = pd.DataFrame(data=pos_tt_press, columns=levels[10:])
 
-        df1 = df1.assign(Dates=neg_dates_d)
-        df2 = df2.assign(Dates=pos_dates_d)
+          df1 = df1.assign(SurfTemp=neg_stemp)
+          df1 = df1.assign(T2M=neg_t2m)
+          df1 = df1.assign(UV=neg_wind)
 
-        df1.to_csv("{0}/CSV_RCP/{4}/{1}_{2}{3:02d}_windpress_neg.csv".format(folder, name, year, month, year))
-        df2.to_csv("{0}/CSV_RCP/{4}/{1}_{2}{3:02d}_windpress_pos.csv".format(folder, name, year, month, year))
+          df2 = df2.assign(SurfTemp=pos_stemp)
+          df2 = df2.assign(T2M=pos_t2m)
+          df2 = df2.assign(UV=pos_wind)
 
-        df1 = pd.DataFrame(data=neg_tt_press, columns=levels[10:])
-        df2 = pd.DataFrame(data=pos_tt_press, columns=levels[10:])
+          df1 = df1.assign(Dates=neg_dates_d)
+          df2 = df2.assign(Dates=pos_dates_d)
 
-        df1 = df1.assign(SurfTemp=neg_stemp)
-        df1 = df1.assign(T2M=neg_t2m)
-        df1 = df1.assign(UV=neg_wind)
-
-        df2 = df2.assign(SurfTemp=pos_stemp)
-        df2 = df2.assign(T2M=pos_t2m)
-        df2 = df2.assign(UV=pos_wind)
-
-        df1 = df1.assign(Dates=neg_dates_d)
-        df2 = df2.assign(Dates=pos_dates_d)
-
-        df1.to_csv("{0}/CSV_RCP/{4}/{1}_{2}{3:02d}_neg.csv".format(folder, name, year, month, year))
-        df2.to_csv("{0}/CSV_RCP/{4}/{1}_{2}{3:02d}_pos.csv".format(folder, name, year, month, year))
+          df1.to_csv("{0}/CSV_RCP/{4}/{1}_{2}{3:02d}_neg.csv".format(folder, name, year, month, year))
+          df2.to_csv("{0}/CSV_RCP/{4}/{1}_{2}{3:02d}_pos.csv".format(folder, name, year, month, year))
   #      sys.exit()
-
-
-
-  # params = stats.exponweib.fit(data, floc=0, f0=1)
-  # shape = params[1]
-  # scale = params[3]
-
-  # print('shape:',shape)
-  # print('scale:',scale)
-
-  # #### Plotting
-  # fig = plt.figure(figsize=(11, 11), frameon=False, dpi=100)
-  # # Histogram first
-  # values,bins,hist = plt.hist(data,bins=51,range=(0,25),density=True)
-  # center = (bins[:-1] + bins[1:]) / 2.
-
-  # # Using all params and the stats function
-  # plt.plot(center,stats.exponweib.pdf(center,*params),lw=4,label='scipy')
-
-  #   #/pixel/project01/cruman/ModelData/PanArctic_0.5d_ERAINT_NOCTEM_RUN/Samples/PanArctic_0.5d_ERAINT_NOCTEM_RUN_198001
-  # plt.legend()
-  # plt.savefig('testea.png')
-  # plt.close()
-
-  # fig = plt.figure(figsize=(11, 11), frameon=False, dpi=100)
-
-  # print(shape.data)
-  # plt.plot(np.arange(len(data)), data)
-
-  # plt.savefig("Teste2a.png")
-  # plt.close()
-
-  # fig = plt.figure(figsize=(11, 11), frameon=False, dpi=100)
-
-  # print(shape.data)
-  # plt.plot(np.arange(len(data)), shf[:,i,j])
-
-  # plt.savefig("Teste_shfa.png")
-  # plt.close()  
-
-
-
 
 def geo_idx(dd, dd_array, type="lat"):
   '''
