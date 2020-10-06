895dbe2fd3cab05c30d40a590c33b3fd00a43572
Caio J Ruman <caioruman@gmail.com>
2020-10-06 18:14:11 -0400

    Comparing data with the soundings

--- sounds-model-dist.py
+++ sounds-model-dist.py
@@ -111,9 +111,9 @@ def readDataSoundings(folder, name, months, datai, dataf):
         aux_tmp = interpolateData(df_aux['TEMP'], levels, df_aux['HGHT'])
         aux_wind = interpolateData(df_aux['SKNT'], levels, df_aux['HGHT'])/1.944
 
-        df_wind_aux = pd.DataFrame(aux_wind, columns=levels)
+        df_wind_aux = pd.DataFrame(data=aux_wind, columns=levels)
         df_wind_aux['Dates'] = dt
-        df_tmp_aux = pd.DataFrame(aux_tmp, columns=levels)
+        df_tmp_aux = pd.DataFrame(data=aux_tmp, columns=levels)
         df_tmp_aux['Dates'] = dt
 
         frames = [df_wind, df_wind_aux]
