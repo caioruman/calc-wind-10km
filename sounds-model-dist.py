c7962f00e3cc9e199bc457951e8e8d3c31628d82
Caio J Ruman <caioruman@gmail.com>
2020-10-07 15:34:16 -0400

    misc

--- sounds-model-dist.py
+++ sounds-model-dist.py
@@ -141,6 +141,9 @@ def readDataSoundings(folder, name, months, datai, dataf):
             continue
 
           aux_inv = df_aux['TEMP'].values[1] - df_aux['TEMP'].values[0]
+          if np.isnan(aux_inv):
+            print(df_aux['TEMP'].values[1], df_aux['TEMP'].values[0])
+            print(df_aux['HGHT'].values[1], df_aux['HGHT'].values[0])
         
           df_wind.loc[i] = aux_wind.tolist() + [aux_inv] + [dt] 
           df_tmp.loc[i] = aux_tmp.tolist() + [aux_inv] + [dt]        
